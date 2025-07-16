6df1318e61dd39851fcb0ef61aca103d70ce222c
kiruthiga0804 <kiruthiga@iitmpravartak.net>
2025-07-16 16:05:35 +0530

    created_by

--- env/schemas.py
+++ env/schemas.py
@@ -43,6 +43,7 @@ class PurchaseOrderCreate(BaseModel):
     quotation_number: Optional[str] = None
     quotation_date: Optional[date] = None  # Optional if needed
     email: Optional[str] = None
+    created_by: str
     dated: Optional[date] = None
     total_cost: int
     total_including_gst: float  # 💡 Add this
@@ -104,6 +105,7 @@ class PurchaseOrder(BaseModel):
     quotation_number: Optional[str]
     quotation_date: Optional[date]
     email: Optional[str]
+    created_by: Optional[str]
     dated: Optional[date]
     additional_terms: Optional[str]
     delivery_date: Optional[date]
@@ -176,13 +178,15 @@ class WorkOrderBase(BaseModel):
         return v or None
 
 class WorkOrderCreate(WorkOrderBase):
+    created_by: str  # ✅ Add this line
     pass
 
 class WorkOrder(WorkOrderBase):
     id: int
     created_at: datetime
     updated_at: datetime
-
+    created_by: Optional[str]
+ 
     class Config:
         orm_mode = True
 
@@ -236,7 +240,7 @@ class TotalOrder(BaseModel):
     id: int
     indent_date: Optional[date]
     orders_date: Optional[date]
-    email: Optional[str]
+    created_by: Optional[str]
     Item_Description: Optional[str]
     Supplier_Name: Optional[str]
     Quantity: Optional[int]
