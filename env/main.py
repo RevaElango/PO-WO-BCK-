5c6b1c35ad583ffb456e0d068bf708bf82e8f822
kiruthiga0804 <kiruthiga@iitmpravartak.net>
2025-08-01 11:19:57 +0530

    corrected

--- env/main.py
+++ env/main.py
@@ -673,6 +673,7 @@ def get_project_keywords(db: Session = Depends(get_db), user: dict = Depends(get
         {
             "id": project.id,
             "project_keyword": project.project_keyword,
+            "project_no":project.project_no,
             "pn_prefix": project.pn_prefix,
             "pn_suffix": project.pn_suffix
         }
